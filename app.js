be38ef6cb73cccc7d95270e360222af15dae8367
jesildebug <jesilpb95@gmail.com>
2023-06-16 09:48:47 +0530

    contact section

--- app.js
+++ app.js
@@ -31,6 +31,14 @@ allSections.addEventListener('click', (e) => {
         const element= document.getElementById(id);
         element.classList.add('active');
     }
+    
+    // toggle theme
+    
+    const themeBtn = document.querySelector('.theme-btn')
+    themeBtn.addEventListener('click' ,() => {
+      let element = document.body;
+      element.classList.toggle('light-mode')
+    })
 })
 
 
